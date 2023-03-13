d303f587c6df686d90978cb90ab0fb03b23d9633
thien113 <n-tv@hotmail.de>
2023-03-13 12:05:46 +0100

    logo svg to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
   return (
     <header>
+      <Image
+        src="/images/crepe.svg"
+        height={50}
+        width={50}
+        alt="crêpe cone log"
+      />
       <nav>
         <Link href="/">Home</Link>
         <Link href="/menu">Menu</Link>
