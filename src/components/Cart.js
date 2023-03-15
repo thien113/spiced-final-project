3e4b50e7a0301df4a5083060d3082edffd4326c9
thien113 <n-tv@hotmail.de>
2023-03-15 16:37:16 +0100

    cart component conditonal rendering

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,31 +1,59 @@
+import Link from "next/link";
+
 export default function Cart({ products }) {
+  console.log(products);
   return (
-    <>
+    <div className="cart">
       <h2>Cart</h2>
-      {products.map((product) => (
-        <div className="cart">
-          <h4>{product.counter}</h4>
-          <h4>{product.name}</h4>
-          <p>{product.price} €</p>
-          <p>
-            {product.extras.map((e) => (
-              <>
-                <span>
-                  <input type="checkbox" /> {e.extra} - {e.price} €
-                </span>
-                <br />
-              </>
-            ))}
-          </p>
-          <label htmlFor="comment">Comment: </label>
-          <input type="text" name="comment" id="comment" />
-        </div>
-      ))}
-      <hr />
-      <input type="checkbox" name="pickup" />
-      <label htmlFor="pickup">Pickup</label>
-      <input type="checkbox" name="delivery" />
-      <label htmlFor="delivery">Delivery</label>
-    </>
+      {products.length === 0 && (
+        <>
+          <div className="cart-items">
+            <p>Your cart is empty...</p>
+          </div>
+        </>
+      )}
+      {products.length != 0 && (
+        <>
+          {products.map((product) => (
+            <div className="cart-items">
+              <div className="cart-items-row">
+                <h4>{product.counter} </h4>
+                <h4>{product.name} </h4>
+                <p>{product.price} €</p>
+              </div>
+              <div>
+                <p>
+                  {product.extras.map((e) => (
+                    <>
+                      <span>
+                        <input type="checkbox" /> {e.extra} - {e.price} €
+                      </span>
+                      <br />
+                    </>
+                  ))}
+                </p>
+                <label htmlFor="comment">Comment: </label>
+                <input type="text" name="comment" id="comment" />
+              </div>
+            </div>
+          ))}
+          <hr />
+          <div className="cart-items-row">
+            <input type="checkbox" name="pickup" />
+            <label htmlFor="pickup">Pickup</label>
+            <input type="checkbox" name="delivery" />
+            <label htmlFor="delivery">Delivery</label>
+          </div>
+          <div className="cart-items">
+            <h3>Total: €</h3>
+            <nav>
+              <Link href="/checkout">
+                <button className="cover-button">Checkout</button>
+              </Link>
+            </nav>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
