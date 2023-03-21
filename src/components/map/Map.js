35a670649f9044cc0dc354e0101bc7740e5d4b61
thien113 <n-tv@hotmail.de>
2023-03-21 13:02:28 +0100

    map and a marker with leaflet

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 export default function Map() {
   const [lat, setLat] = useState(null);
   const [long, setLong] = useState(null);
+  const position = [52.509, 13.416];
 
   function getUserCoordinates() {
     const geolocationAPI = navigator.geolocation;
@@ -23,10 +28,24 @@ export default function Map() {
   }
   getUserCoordinates();
   return (
-    <div className="App">
+    <div>
       <p>Your coordinates are: </p>
       <p>Latitude: {lat}</p>
       <p>Longitude: {long}</p>
+      <MapContainer
+        className="map"
+        center={position}
+        zoom={15}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>Here you are.</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
