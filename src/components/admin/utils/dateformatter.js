27edacbd5848b8c2bd7b3a36d13d6c13305650a5
thien113 <n-tv@hotmail.de>
2023-03-30 15:51:55 +0200

    dateformatter bug fix

--- src/components/admin/utils/dateformatter.js
+++ src/components/admin/utils/dateformatter.js
@@ -1,12 +1,7 @@
-function DateFormatter(date) {
+export default function DateFormatter(date) {
   const day = ("0" + date.getDate()).slice(-2);
   const month = ("0" + (date.getMonth() + 1)).slice(-2);
   const year = date.getFullYear();
   const dateToFilter = `${year}-${month}-${day}`;
   return dateToFilter;
 }
-function DateFormatterFromMongoose(date) {
-  const formatedDate = date.slice(0, 10);
-  return formatedDate;
-}
-export { DateFormatter, DateFormatterFromMongoose };
